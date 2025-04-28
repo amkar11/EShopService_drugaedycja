d795f8480097c6bad794bd2120325382faf573e9
s-A301-30 <s-A301-30@CAMPUS.UEK.KRAKOW.PL>
2025-04-28 20:00:42 +0200

    JWT

---
+++ User.Application/LoginService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+using User.Domain;
+
+namespace User.Application
+{
+    public class LoginService : ILoginService
+    {
+        IJwtTokenService _jwtTokenService;
+        public LoginService(IJwtTokenService jwtTokenService) {
+            _jwtTokenService = jwtTokenService;
+        }
+
+        public string Login(string name, string password)
+        {
+            if (name == "admin" || password == "password")
+            {
+                var roles = new List<string> { "Client", "Employee", "Administrator" };
+                var token = _jwtTokenService.GenerateToken(123, roles);
+                return token;
+            }
+            else
+            {
+                throw new InvalidCredentialsException();
+            }
+        }
+    }
+}
